fc62b5d318b73e5d463e78c83928f80fc0e66734
cieloginzafarm <cielo@ginzafarm.co.jp>
2020-10-08 12:03:56 +0900

    Added some dynamic mapping

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -13,15 +13,30 @@ class MyMap extends Component {
 
     countryStyle = {
         fillOpacity: 0.5,
-    }
+        weight: 2,
+    };
+
+    onEachPrefecture = (prefecture, layer) => {
+      const prefectureName = prefecture.properties.prefectures_name;
+      console.log(prefectureName);
+      layer.bindPopup(prefectureName);
+      layer.on({
+        click: (event) => {
+          event.target.setStyle({
+            color: "green"
+          })
+        }
+      })
+    };
+
     render() { 
         return (
-          <Map center={[33.5, 137]}  zoom={5}>
+          <Map center={[33.5, 137]} zoom={5}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            <GeoJSON style={this.countryStyle}data={japanData.features} />
+            <GeoJSON style={this.countryStyle} data={japanData.features} onEachFeature={this.onEachPrefecture}/>
           </Map>
         );
     }
